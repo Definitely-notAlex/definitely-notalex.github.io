8992836afbb62e68b015c65854acece87141467b
YepNotAlex <alex@skelton007.plus.com>
2026-01-27 12:42:07 +0000

    Location instead of open

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const Lab2 = document.getElementById("Lab2");
 // Switch between images when the button is clicked
 Lab1.addEventListener("click", function () {
 
-    window.open("https://definitely-notalex.github.io/ARCHOSC-Lab1/");
+    window.location = "https://definitely-notalex.github.io/ARCHOSC-Lab1/";
 
 });
 
