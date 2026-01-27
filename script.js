000f64e660860dbe4b943597e675acb5ab6c735c
YepNotAlex <alex@skelton007.plus.com>
2026-01-27 12:26:16 +0000

    Add Front Page Navigation

--- script.js
+++ script.js
@@ -1,27 +1,16 @@
-// Paths to the two images
-const image1 = "Pictures/001.png";
-const image2 = "Pictures/002.png";
-
-// Keeps track of which image is currently shown
-let showingFirstImage = true;
-
 // Get references to HTML elements
-const displayedImage = document.getElementById("displayedImage");
-const switchButton = document.getElementById("switchButton");
-
-// Set the initial image when the page loads
-displayedImage.src = image1;
+const Lab1 = document.getElementById("Lab1");
+const Lab2 = document.getElementById("Lab2");
 
 // Switch between images when the button is clicked
-switchButton.addEventListener("click", function () {
+Lab1.addEventListener("click", function () {
 
-    // Check which image is currently displayed and switch
-    if (showingFirstImage) {
-        displayedImage.src = image2;
-    } else {
-        displayedImage.src = image1;
-    }
+    window.open("https://definitely-notalex.github.io/ARCHOSC-Lab1/");
 
-    // Flip the boolean so next click switches again
-    showingFirstImage = !showingFirstImage;
+});
+
+Lab2.addEventListener("click", function () {
+
+    //window.open("https://definitely-notalex.github.io/ARCHOSC-Lab1/");
+
 });
