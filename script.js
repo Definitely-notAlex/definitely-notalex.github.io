9fe8e00255f87634340e30995c86a848f955b6e4
YepNotAlex <alex@skelton007.plus.com>
2026-02-10 11:33:56 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // Get references to HTML elements
 const Lab1 = document.getElementById("Lab1");
 const Lab2 = document.getElementById("Lab2");
+const Lab3 = document.getElementById("Lab3");
 
 // Switch between images when the button is clicked
 Lab1.addEventListener("click", function () {
@@ -11,8 +12,16 @@ Lab1.addEventListener("click", function () {
 
 Lab2.addEventListener("click", function () {
 
-    window.locate("https://definitely-notalex.github.io/ARCHOSC-Lab2/");
+    window.location = "https://definitely-notalex.github.io/ARCHOSC-Lab2/";
 
 
 });
+
+Lab3.addEventListener("click", function () {
+
+    window.location = "https://definitely-notalex.github.io/ARCHOSC-Lab3/";
+
+
+});
+
 
